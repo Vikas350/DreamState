ddb590c03a399dfa694bb4922da68fca45d62c3e
Vikas Kushwaha <vikas@skylarkdrones.com>
2024-03-30 13:53:52 +0530

    feat: Connect to the database

--- server/index.js
+++ server/index.js
@@ -1,7 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
 
+// config env variables
+dotenv.config()
+ 
+//create express app
 const app = express();
+const port = process.env.PORT
 
-app.listen(3000, () => {
+//connect the mongodb database
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('MongoDB database is connected to the server.')
+}).catch((err) => {
+    console.log(err)
+})
+
+// start the server on PORT
+app.listen(port, () => {
     console.log('Server is running on port 3000')
 })
