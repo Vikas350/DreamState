9c1273524438f5dd2c3202f134ac4963d6a2d459
Vikas Kushwaha <vikas@skylarkdrones.com>
2024-03-31 18:39:05 +0530

    feat: create Google OAuth component

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -15,6 +15,10 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
+    },
+    avatar: {
+        type: String,
+        default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdepositphotos.com%2Fvectors%2Fuser-profile.html&psig=AOvVaw1cgEX4VgcDvxw8XCrhR_PR&ust=1711975931180000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNCJgb7FnoUDFQAAAAAdAAAAABAE"
     }
 },{timestamps: true}) //add extra info to the data(creation and updation time) 
 
