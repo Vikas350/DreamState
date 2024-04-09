cc7d10293b802792733aeb4c1a205a788f384eed
Vikas Kushwaha <vikas@skylarkdrones.com>
2024-04-10 00:39:19 +0530

    feat: Add search listing route

--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -66,8 +66,59 @@ export const getListing = async (req, res, next) => {
     if (!listing) {
       return next(errorHandler(404, "Listing not found!"));
     }
-    
+
     res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getListings = async (req, res, next) => {
+  try {
+    //first here we create pagination for searching using the listings limit
+    const limit = parseInt(req.query.limit) || 9; // limit for one page
+    const startIndex = parseInt(req.query.startIndex) || 0; // skip variable
+
+    // if the offer is in query or not if its not true show all listing whether true or false
+    let offer = req.query.offer;
+    if (offer === undefined || offer === "false") {
+      offer = { $in: [false, true] };
+    }
+
+    let furnished = req.query.furnished;
+    if (furnished === undefined || furnished === "false") {
+      furnished = { $in: [false, true] };
+    }
+
+    let parking = req.query.parking;
+    if (parking === undefined || parking === "false") {
+      parking = { $in: [false, true] };
+    }
+
+    let type = req.query.type;
+    if (type === undefined || type === "all") {
+      type = { $in: ["sale", "rent"] };
+    }
+
+    const searchTerm = req.query.searchTerm || "";
+
+    const sort = req.query.sort || "createdAt";
+    const order = req.query.order || "desc";
+
+    // $regex - helps in searching => search at every whether in the string
+    // $options: "i" => we does not need to care about small/largecase
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: "i" },
+      offer,
+      furnished,
+      parking,
+      type,
+    })
+      .sort({ [sort]: order })
+      .limit(limit)
+      .skip(startIndex);
+
+    return res.status(200).json(listings);
   } catch (error) {
     next(error);
   }
