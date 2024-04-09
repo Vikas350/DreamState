7954a5bdafdc3eb3494ecb21cdade240c2015f47
Vikas Kushwaha <vikas@skylarkdrones.com>
2024-04-10 00:58:47 +0530

    feat: Update header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa"; // icons from font awesomw website
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom"; // link to other route without refresh the page
+import { Link, useNavigate } from "react-router-dom"; // link to other route without refresh the page
 
 function Header() {
-  const {currentUser} = useSelector(state => state.user);
+  const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate()
+
+  // we wanted to keep the search query urk updated on changing input valu and vice versa
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const urlParams = new URLSearchParams(window.location.search)
+    urlParams.set('searchTerm', searchTerm)
+    const searchQuery = urlParams.toString()
+    navigate(`/search?${searchQuery}`)
+  }
+
+  // and we want to change the input value if we chnage the query and keep other query params 
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search)
+    const searchTermFromUrl = urlParams.get('searchTerm')
+    if(searchTermFromUrl){
+      setSearchTerm(searchTermFromUrl)
+    }
+  }, [location.search])
 
   return (
     <header className="bg-green-100 shadow-md">
@@ -15,13 +35,17 @@ function Header() {
             <span className="text-green-700">State</span>
           </h1>
         </Link>
-        <form className="bg-green-50 p-3 rounded-lg flex items-center">
+        <form onSubmit={handleSubmit} className="bg-green-50 p-3 rounded-lg flex items-center">
           <input
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <FaSearch className="text-green-600" />
+          <button>
+            <FaSearch className="text-green-600" />
+          </button>
         </form>
         <ul className="flex gap-4">
           <Link to="/">
@@ -36,7 +60,11 @@ function Header() {
           </Link>
           <Link to="/profile">
             {currentUser ? (
-              <img className="rounded-full h-7 w-7 object-cover" src={currentUser.avatar}  alt="profile_image"/>
+              <img
+                className="rounded-full h-7 w-7 object-cover"
+                src={currentUser.avatar}
+                alt="profile_image"
+              />
             ) : (
               <li className="text-green-700 hover:underline">SignIn</li>
             )}
